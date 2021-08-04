1cb42be38af0bee471e4f90a3d8deb1aa95f5222
Pablo <pablopst@gmail.com>
2021-08-04 19:56:03 -0300

    added global style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
-import styles from '../styles/home.module.scss';
-
+import Head from 'next/head';
 
 export default function Home() {
-  return <h1 className={styles.title}>Hello world</h1>
+  return (
+    <>
+      <Head>
+        <title>
+          inicio
+        </title>
+      </Head>
+    <h1>
+      Hello world
+    </h1>
+    </>
+  )
 }
