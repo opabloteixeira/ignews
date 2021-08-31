d60b049a360935a19ea6f23351db276e819420f5
Pablo <pablopst@gmail.com>
2021-08-31 20:20:50 -0300

    added infos for  home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,28 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss'
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>
-          Home
-
-          
+          Home | ig.news
         </title>
-      </Head>
+      </Head> 
+        <main className={styles.contentContainer}>
+          <section className={styles.hero}>
+            <span>
+            👏 Hew, welcome
+            </span>
+            <h1>New about the <span>React</span> world.</h1>
+            <p>
+              get access to all the publications <br />
+              <span>for $9.90 month</span>
+            </p>
+          </section>
+          <img src="/images/avatar.svg" alt="Girl coding" />
+        </main>
     
     </>
   )
